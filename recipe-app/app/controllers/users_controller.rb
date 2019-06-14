5a40f11276c8c1d60a99e72e442129d3fd8d1a94
lieby weintraub <liebahinda@gmail.com>
2019-06-14 01:59:42 +0000

    add code, fix error

--- recipe-app/app/controllers/users_controller.rb
+++ recipe-app/app/controllers/users_controller.rb
@@ -34,9 +34,15 @@ user = User.find_by(:username => params[:username])
       session[:user_id] = user.id
       redirect to '/recipes'
     else
-      redirect to '/login'
+      erb :failure
     end
   end
+  
+  get "/logout" do 
+    session.clear 
+    redirect "login"
+    
+  end 
 
 
 
@@ -47,7 +53,8 @@ user = User.find_by(:username => params[:username])
   @recipes= @user.recipes
     erb :"/users/show.html"
   end
-end
+  
+  end
 
  
 
